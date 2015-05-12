120e9f9e26dbbfd6a690333533720df1eb010e86
SergeOxe <sergeoxe@gmail.com>
2015-05-12 20:36:24 +0300

    port80

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ app.get('/', function(request, response) {
 });
 
 
-var server = app.listen(5000, function () {
+var server = app.listen(80, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
