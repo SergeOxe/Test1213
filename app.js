af3dfa615cf7ec0b62f4a743963f7c37feea2b4f
SergeOxe <sergeoxe@gmail.com>
2015-05-12 20:38:49 +0300

    3000

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ app.get('/', function(request, response) {
 });
 
 
-var server = app.listen(80, function () {
+var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
