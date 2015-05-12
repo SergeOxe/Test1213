5abeee5c4b70c9743c7688a4d79a9a13a7bbae80
SergeOxe <sergeoxe@gmail.com>
2015-05-12 20:03:42 +0300

    Boom

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.get('/', function(request, response) {
 
 app.set('port', (process.env.PORT || 3000));
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
