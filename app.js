378c7c3583f1e159ee4bc02d09f54af349abe4aa
SergeOxe <sergeoxe@gmail.com>
2015-06-01 20:04:20 +0300

    app

--- app.js
+++ app.js
@@ -145,8 +145,14 @@ app.post('/addValueToTeam', function (req, res) {
     reqHandler.addValueToTeam(req.body.json,res);
 });
 
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+});
 
-var server = app.listen(3000, function () {
+app.set('port', (process.env.PORT || 5000));
+
+var server = app.listen(app.get('port'), function () {
+//var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
